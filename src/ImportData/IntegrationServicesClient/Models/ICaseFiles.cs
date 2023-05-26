a9f451f3e54d3decf1cec162802026d0e254febf
Nevolin_DA <Nevolin_DA@DIRECTUM.ru>
2023-05-26 18:10:14 +0400

    Добавил импорт кейс файлов

---
+++ src/ImportData/IntegrationServicesClient/Models/ICaseFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportData.IntegrationServicesClient.Models
+{
+    [EntityName("Дело")]
+    public class ICaseFiles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Index { get; set; }
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+        public string Note { get; set; }
+        public IDepartments Department { get; set; }
+        public IFileRetentionPeriods RetentionPeriod { get; set; }
+        public bool LongTerm { get; set; }
+        public string Location { get; set; }
+        public IRegistrationGroups RegistrationGroup { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public IBusinessUnits BusinessUnit { get; set; }
+    }
+}
